5fc35b43ba20170df617d83495489688c6dd72f9
Kishor Jabegu <Kishor.Jabegu@coalservices.com.au>
2021-09-11 09:02:54 +1000

    completed set up and updated git hub

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# eCommerceAdminSection
+# Ecommerce admin cms
+
+This project is for the admin panel of the ecommerce site.
+
+## How to run
+
+- Clone the project
+- Run `npm install`
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {Button} from 'react-bootstrap'
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      Starting Admin CMS
+      <Button variant='success'> test button</Button>
+      <i className="fab fa-react"></i>
+    </div>
+  );
+}
+
+export default App;
